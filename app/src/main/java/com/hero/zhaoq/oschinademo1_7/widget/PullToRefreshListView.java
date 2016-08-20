f43ea87d892795298e5cf7b1448e55ed7e9839e3
zhaoQiang <229457269@qq.com>
2016-08-20 22:37:42 +0800

    继续写pullToRefreshListView控件

--- app/src/main/java/com/hero/zhaoq/oschinademo1_7/widget/PullToRefreshListView.java
+++ app/src/main/java/com/hero/zhaoq/oschinademo1_7/widget/PullToRefreshListView.java
@@ -3,12 +3,16 @@ package com.hero.zhaoq.oschinademo1_7.widget;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
 import android.widget.AbsListView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.hero.zhaoq.oschinademo1_7.R;
 
 /**
  * Package_name:com.hero.zhaoq.oschinademo1_7.widget
@@ -47,12 +51,49 @@ public class PullToRefreshListView extends ListView implements AbsListView.OnScr
         super(context, attrs, defStyleAttr);
         init(context);//用于  在xml文件中初始化布局
     }
-
+    //用来设置  箭头图标动画效果
+    private RotateAnimation animation;
+    private RotateAnimation reverseAnimation;
+    private int headContentWidth;
+    private int headContentHeight;
     //初始化  布局
     private void init(Context context) {
+        //Interpolator用于动画中的时间插值，其作用就是把0到1的浮点值变化映射到另一个浮点值变化。
         //设置滑动效果：
+        animation = new RotateAnimation(0,-180,RotateAnimation.RELATIVE_TO_SELF,0.5f,
+                RotateAnimation.RELATIVE_TO_SELF,0.5f);
+        animation.setInterpolator(new LinearInterpolator());  //设置  差值器
+        animation.setDuration(100);
+        animation.setFillAfter(true);
 
+        reverseAnimation = new RotateAnimation(-180,0,
+                RotateAnimation.RELATIVE_TO_SELF,0.5f,
+                RotateAnimation.RELATIVE_TO_SELF,0.5f);
+        reverseAnimation.setInterpolator(new LinearInterpolator());
+        reverseAnimation.setDuration(100);
+        reverseAnimation.setFillAfter(true);
 
+        inflater = LayoutInflater.from(context);
+        headView = (LinearLayout) inflater.inflate(R.layout.pull_to_refresh_head,null);
+
+        arrowImageView = (ImageView) headView.findViewById(R.id.head_arrowImageView);
+        arrowImageView.setMinimumHeight(50);
+        arrowImageView.setMinimumWidth(50);
+        progressBar = (ProgressBar) headView.findViewById(R.id.head_progressBar);
+        tipsTextView = (TextView) headView.findViewById(R.id.head_tipsTextView);
+        lastUpdatedTextView = (TextView) headView.findViewById(R.id.head_lastUpdatedTextView);
+
+        measureView(headView);
+        headContentHeight =  headView.getMeasuredHeight();  //获得  测量的高度
+        headContentWidth = headView.getMeasuredWidth(); //获得  测量的宽度
+
+        headView.setPadding(headView.getPaddingLeft(),-1 * headContentHeight,
+                headView.getPaddingRight(),headView.getPaddingBottom());
+        headView.invalidate();
+
+        //添加   进头视图中
+        addHeaderView(headView);
+        setOnScrollListener(this);
     }
 
     @Override
